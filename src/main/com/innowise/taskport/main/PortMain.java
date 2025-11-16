501e39ef54fdca551d53d06e89088348c3f31c15
PavelDulskij <paveldulskij06@gmail.com>
2025-11-16 18:19:32 +0300

    Update PortMain.java

--- src/main/com/innowise/taskport/main/PortMain.java
+++ src/main/com/innowise/taskport/main/PortMain.java
@@ -1,14 +1,7 @@
 package com.innowise.taskport.main;
 
-import com.innowise.taskport.entity.Ship;
-import com.innowise.taskport.exception.PortException;
-import com.innowise.taskport.warehouse.Warehouse;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class PortMain {
-    static void main() throws PortException {
-        
+    static void main() {
+
     }
 }
